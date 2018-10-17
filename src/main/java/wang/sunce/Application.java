e140dd7f01579425de555c8fcb52603db9882ef1
sunce <sexylowrie@163.com>
2018-10-17 14:58:23 +0800

    提交测试类

--- src/main/java/wang/sunce/Application.java
+++ src/main/java/wang/sunce/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 项目启动类
@@ -18,9 +20,15 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableDiscoveryClient
 @EnableFeignClients
 @ServletComponentScan
+@RestController
 @MapperScan(basePackages = { "wang.sunce.**.dao" })
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
     }
 }
