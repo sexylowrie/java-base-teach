a1b58f6f51a8089080aa6720e815a0892d6c5b9d
sunce <king@sunce.xin>
2018-12-23 12:39:06 +0800

    介绍基本类型以及其运算

---
+++ src/main/java/wang/sunce/teach/base/chapter1/CalculateDemo2.java
@@ -0,0 +1,15 @@
+package wang.sunce.teach.base.chapter1;
+
+public class CalculateDemo2 {
+
+    public static void main(String[] args) {
+
+        short s = 1;
+        // 默认有int --> short 强制类型转换
+        s += 1;
+
+        //s = s + 1;//不会有类型转换
+
+    }
+
+}
