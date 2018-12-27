6f0fe513e9e483e94bff08cab3772f7a63f4f6a1
sunce <king@sunce.xin>
2018-12-27 13:36:44 +0800

    新增备受关注的字符串

--- src/main/java/wang/sunce/teach/base/chapter4/StringDemo2.java
+++ src/main/java/wang/sunce/teach/base/chapter4/StringDemo2.java
@@ -12,15 +12,5 @@ public class StringDemo2 {
         System.out.println(str1.intern() == str3.intern());
         System.out.println(str1.intern() == str4.toString().intern());
 
-        String strX = new StringBuilder("计算机").toString();
-        System.out.println(strX.intern() == strX);
-
-        String str11 = new StringBuilder("计算").append("软件").toString();
-        System.out.println(str11.intern() == str11);
-
-        String str21 = new StringBuilder("ja").append("va").toString();
-        System.out.println(str21.intern() == str21);
-
-
     }
 }
