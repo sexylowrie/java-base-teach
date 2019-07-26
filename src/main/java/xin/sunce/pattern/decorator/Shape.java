e71a4a75a2567c659508d5f7762023d813f3e7e8
sunce <king@sunce.xin>
2019-07-26 23:52:11 +0800

    新增装饰器模式

---
+++ src/main/java/xin/sunce/pattern/decorator/Shape.java
@@ -0,0 +1,16 @@
+package xin.sunce.pattern.decorator;
+
+/**
+ * 组件
+ *
+ * @author lowrie
+ * @date 2019-07-26
+ */
+public interface Shape {
+
+    /**
+     * 绘画方法
+     */
+    void draw();
+
+}
