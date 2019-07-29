5ee9452b8489ea9020e16da674b9ad5863c886ac
sunce <king@sunce.xin>
2019-07-29 08:53:12 +0800

    委派模式

---
+++ src/main/java/xin/sunce/pattern/delegate/DelegateWorker.java
@@ -0,0 +1,22 @@
+package xin.sunce.pattern.delegate;
+
+/**
+ * 委派者
+ *
+ * @author lowrie
+ * @date 2019-07-28
+ */
+public class DelegateWorker implements Worker {
+
+    private Worker worker;
+
+    public DelegateWorker(Worker worker) {
+        this.worker = worker;
+    }
+
+    @Override
+    public void doWork() {
+        System.out.print("Hi Boss,");
+        worker.doWork();
+    }
+}
---
+++ src/main/java/xin/sunce/pattern/delegate/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 委派模式
+ *
+ * @author lowrie
+ * @date 2019-07-28
+ */
+package xin.sunce.pattern.delegate;
