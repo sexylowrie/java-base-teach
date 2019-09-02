522507e5f30e4439aa87f5a3b32a9293bd153444
sunce <king@sunce.xin>
2019-09-02 14:36:08 +0800

    设计模式更新

--- src/main/java/xin/sunce/pattern/decorator/package-info.java
+++ src/main/java/xin/sunce/pattern/decorator/package-info.java
@@ -1,9 +1,9 @@
 /**
- * 代理模式
+ * 装饰器模式
  * <p>
  * 结构型设计模式
  *
  * @author lowrie
  * @date 2019-09-02
  */
-package xin.sunce.pattern.proxy;
+package xin.sunce.pattern.decorator;
--- src/main/java/xin/sunce/pattern/singleton/package-info.java
+++ src/main/java/xin/sunce/pattern/singleton/package-info.java
@@ -1,9 +1,9 @@
 /**
- * 工厂模式
+ * 单例模式
  * <p>
  * 创建型设计模式
  *
  * @author lowrie
  * @date 2019-09-02
  */
-package xin.sunce.pattern.factory;
+package xin.sunce.pattern.singleton;
