cbb85177ff2b31deab93a08824796d91f3bcff18
sunce <king@sunce.xin>
2020-04-14 16:49:53 +0800

    新增MyArrayList

--- src/main/java/xin/sunce/chapter7/algorithm/MyArrayList.java
+++ src/main/java/xin/sunce/chapter7/algorithm/MyArrayList.java
@@ -115,7 +115,7 @@ public class MyArrayList {
     }
 
     public static void main(String[] args) {
-        MyArrayList list = new MyArrayList(22);
+        MyArrayList list = new MyArrayList();
         list.add(1);
         list.add(2);
         list.add(3);
